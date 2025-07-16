ecb66e5b767d887bc9e4b2d7ecdc8e6edee94a63
bigit22 <koresh.yar@mail.ru>
2025-07-16 17:36:24 +0700

    create argparser

--- src/parsers.py
+++ src/parsers.py
@@ -1,3 +1,16 @@
+from argparse import Namespace, ArgumentParser
+from typing import Optional
+
+
 class ArgParser:
     def __init__(self):
-        ...
+        self.args: Optional[Namespace] = None
+
+    @staticmethod
+    def parse_args(self, args) -> Namespace:
+        parser: ArgumentParser = ArgumentParser()
+        parser.add_argument('--file', help='Path to the CSV file')
+        parser.add_argument('--where', help='Filter rows by condition')
+        parser.add_argument('--aggregate', help='Aggregate rows by condition')
+        self.args = parser.parse_args(args)
+        return self.args
